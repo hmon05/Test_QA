971df6fa799835b2130867e5f0268628a657ed75
hmon05 <hmontanobel@gmail.com>
2025-03-20 15:19:53 -0500

    teset

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,20 +4,14 @@ test.beforeEach(async ({page}) => {
   await page.goto('https://es.aliexpress.com/');
 });
 
-test.describe( 'test#1' , () => {
+test.describe( 'Agregar un producto al carro de compras:' , () => {
   test('Buscar producto', async ({ page }) => {
       await page.locator('.search--keyword--15P08Ji').fill('Humificador');
-      await page.locator('.search--keyword--15P08Ji').press('Enter');
-      
-  });
-});
-
-test.describe( 'test#2' , () => {
-  test('enter text2', async ({ page }) => {
-      await page.locator('.new-todo').fill('text testing #2');
-      await page.locator('.new-todo').press('Enter');
-
-      await expect(page.locator('.view label')).toHaveText('text testing #2');
+      await page.locator('.search--keyword--15P08Ji').press('Enter');  
+      await page.pause();
+      await page.getByRole('link', { name: 'Report fraud item Rociador de' }).click();
+      // await page.getByRole('link', { name: 'Report fraud item Humidificador de niebla en aerosol portátil, Mini taza de' }).click();
+      // await page.getByTestId('l5_b io_it search-card-item l5_l6').click();
   });
 });
 
