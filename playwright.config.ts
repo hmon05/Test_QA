53b163366b7ac0d1f451c79572081e1a39eba755
hmon05 <hmontanobel@gmail.com>
2025-03-20 15:37:02 -0500

    config t

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,12 +29,12 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-first-failure',
     screenshot: {
-      mode: 'on',
+      mode: 'only-on-failure',
       fullPage: true,
     },
-    video: 'on',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
