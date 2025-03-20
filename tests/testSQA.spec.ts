669dbf731eac073a64d13776f8d7457cf6f5b08b
hmon05 <hmontanobel@gmail.com>
2025-03-20 16:02:33 -0500

    update 2

--- tests/testSQA.spec.ts
+++ tests/testSQA.spec.ts
@@ -8,14 +8,15 @@ test.describe( 'Agregar un producto al carro de compras:' , () => {
   test('Buscar producto', async ({ page }) => {
       await page.locator('.search--keyword--15P08Ji').fill('Humificador');
       await page.locator('input[type="button"]').click(); 
-      // await page.pause();
+
       const page1Promise = page.waitForEvent('popup');
       await page.getByRole('link', { name: 'Report fraud item Humidificador de niebla en aerosol portátil, Mini taza de' }).click();
       const page1 = await page1Promise;
       const precio = await page1.getByText('COP7.336,51').textContent();
+
       await page1.getByRole('link', { name: '0', exact: true }).click();
       expect (precio).toEqual('COP7.336,51');
-      // await page1.locator('div').filter({ hasText: /^Estimación totalCOP7\.336,51$/ }).locator('span').click();
+
   });
 });
 
